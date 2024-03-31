d89491fa299c586b33ca20833cf89e0b458c54e9
carlosveg <carlosvegapro@gmail.com>
2024-03-31 13:21:40 -0600

    feat: Creation of module and service to upload files with aws-s3

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminUser, Pet, ShelterEntity, User, ocupationEntity, shelterRequest , Quality} from './shared/entities/index';
 import { AuthModule } from './modules/auth/auth.module';
 import { ShelterModule } from './modules/shelter/shelter.module';
+import { S3Module } from './modules/s3/s3.module';
 
 
 @Module({
@@ -29,7 +30,8 @@ import { ShelterModule } from './modules/shelter/shelter.module';
       ],
       synchronize:true
 
-    })
+    }),
+    S3Module
   ],
   controllers: [AppController],
   providers: [AppService],
